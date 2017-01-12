e5e34c84a2f6505ff6b15d574e9b7557ef9f4939
Catalin Bradescu <cbradescu@yahoo.com>
2017-01-12 16:24:00 +0200

    Confirm Mass Action info - withoout INSET.

--- src/CB/Bundle/NewAgeBundle/Resources/public/js/datagrid/action/confirm-mass-action.js
+++ src/CB/Bundle/NewAgeBundle/Resources/public/js/datagrid/action/confirm-mass-action.js
@@ -16,6 +16,56 @@ define([
      * @extends oro.datagrid.action.MassAction
      */
     ConfirmAction = MassAction.extend({
+        /** @property {Object} */
+        defaultMessages: {
+            confirm_title: 'Mass Action Confirmation',
+            confirm_content: 'Are you sure you want to do this?',
+            confirm_ok: 'Yes, do it',
+            confirm_cancel: 'Cancel',
+            success: 'Mass action performed.',
+            error: 'Mass action is not performed.',
+            empty_selection: 'Please, select items to perform mass action.',
+            confirmed_selection: 'You have confirmed already confirmed panel views.'
+        },
+
+
+        /**
+         * Ask a confirmation and execute mass action.
+         */
+        execute: function() {
+            if (this.checkSelectionState()) {
+                MassAction.__super__.execute.call(this);
+            }
+        },
+
+        /**
+         * Checks if any records are selected.
+         *
+         * @returns {boolean}
+         */
+        checkSelectionState: function() {
+            var selectionState = this.datagrid.getSelectionState();
+
+            if (selectionState.selectedIds.length === 0 && selectionState.inset) {
+                messenger.notificationFlashMessage('warning', __(this.messages.empty_selection));
+                return false;
+            } else {
+                var data = this.datagrid.collection.models;
+                var selectedIds = {};
+                _.each(selectionState.selectedIds, function (sel) { selectedIds[sel] = true; });
+
+                var confirmedIds = _.filter(data, function (val) {
+                    var available = val.attributes['available'];
+                    return selectedIds[val.id] && ~available.indexOf('badge-disable');
+                });
+
+                if (confirmedIds.length > 0) {
+                    messenger.notificationFlashMessage('danger', __(this.messages.confirmed_selection));
+                    return false;
+                }
+            }
+            return false;
+        },
 
     });
 
