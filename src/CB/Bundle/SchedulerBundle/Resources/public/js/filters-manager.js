3fc8f5ef9c087a605a944e1384a997ca6472c909
Catalin Bradescu <cbradescu@yahoo.com>
2017-02-20 12:38:04 +0200

    Added changes to filters-manager.js

--- src/CB/Bundle/SchedulerBundle/Resources/public/js/filters-manager.js
+++ src/CB/Bundle/SchedulerBundle/Resources/public/js/filters-manager.js
@@ -173,6 +173,32 @@ define(function(require) {
          * @protected
          */
         _onFilterUpdated: function(filter) {
+            // start
+            var activeFilters = [];
+
+            _.each(this.filters, function(filter, name) {
+                if (filter.enabled && !_.isObject(filter.value.value) && filter.value.value) {
+                    activeFilters.push({name: name, value: filter.value.value});
+                }
+
+                if (_.isObject(filter.value.value)) {
+                    switch (name) {
+                        case 'city':
+                        case 'supportType':
+                        case 'lightingType':
+                            var value = filter.value.value.join();
+
+                            if (value) {
+                                activeFilters.push({name: name, value: value});
+                            }
+                            break;
+                    }
+                }
+            }, this);
+
+            mediator.trigger('setSchedulerFilters', activeFilters);
+            // end
+
             this._resetHintContainer();
             this.trigger('updateFilter', filter);
         },
